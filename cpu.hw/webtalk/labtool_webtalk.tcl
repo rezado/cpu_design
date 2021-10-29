de4dd8a000a20ddea404a28949897ac828f1d7cc
rezado <1908819406@qq.com>
2021-10-29 14:39:18 +0800

    增加指令规范doc

--- cpu.hw/webtalk/labtool_webtalk.tcl
+++ cpu.hw/webtalk/labtool_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir D:/Codes/Vivado/2016/cpu/cpu.hw/webtalk/
+webtalk_init -webtalk_dir D:/Codes/cpu/cpu.hw/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Oct 28 22:44:20 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Fri Oct 29 14:35:14 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "Vivado v2016.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "1577090" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,19 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "0a498b3a-d371-4600-9858-28e2cd802f77" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "80e160d6-d50a-4d9b-bba9-e98bae9d809b" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
-webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
-webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
-webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-8265U CPU @ 1.60GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "1800 MHz" -context "user_environment"
-webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
-webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
-webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
-webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
-webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-8265U CPU @ 1.60GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "1800 MHz" -context "user_environment"
-webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
-webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
+webtalk_add_data -client project -key project_iteration -value "7" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-8265U CPU @ 1.60GHz" -context "user_environment"
@@ -36,7 +24,5 @@ webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "use
 webtalk_register_client -client labtool
 webtalk_add_data -client labtool -key pgmcnt -value "00:00:00" -context "labtool\\usage"
 webtalk_add_data -client labtool -key cable -value "" -context "labtool\\usage"
-webtalk_add_data -client labtool -key cable -value "" -context "labtool\\usage"
-webtalk_add_data -client labtool -key cable -value "" -context "labtool\\usage"
-webtalk_transmit -clientid 2180314558 -regid "" -xml D:/Codes/Vivado/2016/cpu/cpu.hw/webtalk/usage_statistics_ext_labtool.xml -html D:/Codes/Vivado/2016/cpu/cpu.hw/webtalk/usage_statistics_ext_labtool.html -wdm D:/Codes/Vivado/2016/cpu/cpu.hw/webtalk/usage_statistics_ext_labtool.wdm -intro "<H3>LABTOOL Usage Report</H3><BR>"
+webtalk_transmit -clientid 2260207815 -regid "" -xml D:/Codes/cpu/cpu.hw/webtalk/usage_statistics_ext_labtool.xml -html D:/Codes/cpu/cpu.hw/webtalk/usage_statistics_ext_labtool.html -wdm D:/Codes/cpu/cpu.hw/webtalk/usage_statistics_ext_labtool.wdm -intro "<H3>LABTOOL Usage Report</H3><BR>"
 webtalk_terminate
